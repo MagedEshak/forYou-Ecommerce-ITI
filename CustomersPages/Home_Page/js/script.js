b7d36c0a4f97e1cf4f9ae7c6b039a101d022107c
Ali Gamal <aligamaldakhly99@gmail.com>
2025-03-29 16:17:48 +0200

    added products section , but still under developement

--- CustomersPages/Home_Page/js/script.js
+++ CustomersPages/Home_Page/js/script.js
@@ -1,3 +1,5 @@
+
+/* this code handels our crusoal and its background images */
 let backgroundImagesUrls = [
     '../images/carusoal_images/1.jpg' , 
     '../images/carusoal_images/2.jpg',
@@ -11,6 +13,8 @@ let controlNextBtn = document.getElementById('controlNextBtn_id'); // next btn
 let counter = 0; // counter to track my background images
 
 controlPrevBtn.addEventListener('click' , ()=>{getPrevImage();})
+controlNextBtn.addEventListener('click' , ()=>{getNextImage();})
+
 function getPrevImage(){
     if(counter <= 0)
         counter = backgroundImagesUrls.length - 1;
@@ -19,9 +23,6 @@ function getPrevImage(){
     
     MyCarousel.style.backgroundImage = `url(${backgroundImagesUrls[counter]})` ;
 }
-
-
-controlNextBtn.addEventListener('click' , ()=>{getNextImage();})
 function getNextImage(){
     if(counter >= backgroundImagesUrls.length - 1)
         counter = 0;
@@ -29,4 +30,59 @@ function getNextImage(){
         counter++;
     
     MyCarousel.style.backgroundImage = `url(${backgroundImagesUrls[counter]})` ;
+}
+
+
+/* this code handels our add to cart btn */
+/* when the button clicked , it must display none ,
+and the productCountAndBin must appear */
+
+let addToCartBtn = document.getElementById('addToCartBtn_id_1'); // cart button
+let productCountAndBinDiv = document.getElementById('productCountAndBin_id_1'); // container of product count and bin
+let productCountSpan = document.getElementById('productCount_id_1');// the span that dispalys product count
+let incrementProductCountBtn = document.getElementById('incrementProductCountBtn_id_1');// + sign to increment prodduct count
+let removeProductCartBtn = document.getElementById('removeProductCartBtn_id_1');// bin to remove the product from cart
+let decrementProductCountBtn = document.getElementById('decrementProductCountBtn_id_1');// - sign to decrement prodduct count
+
+/* handeling when add to cart pressed */
+addToCartBtn.addEventListener('click' , (e) => {
+    displayNone(addToCartBtn); // remove add to cart button from the page
+    display(productCountAndBinDiv); // add the div of count and bin instead
+    console.log(++productCountSpan.innerHTML)// when the button clicked , count of product = 1
+})
+
+/* handeling when + sign pressed */
+incrementProductCountBtn.addEventListener('click',()=>{
+    // when the button clicked increment product count and display minus sign instead of bin
+    console.log(++productCountSpan.innerHTML);
+    displayNone(removeProductCartBtn);//remove my bin from the div
+    display(decrementProductCountBtn);//dispaly - sign instead
+})
+
+/* handeling when - sign pressed */
+decrementProductCountBtn.addEventListener('click' , ()=>{
+    if(productCountSpan.innerHTML == 2){
+        productCountSpan.innerHTML--;
+        displayNone(decrementProductCountBtn);//remove my bin from the div
+        display(removeProductCartBtn);//dispaly - sign instead
+    }
+    else
+        productCountSpan.innerHTML--;
+})
+
+/* handeling when bin btn pressed */
+removeProductCartBtn.addEventListener('click',()=>{
+    productCountSpan.innerHTML--;
+    displayNone(productCountAndBinDiv); // remove add to cart button from the page
+    display(addToCartBtn); // add the div of count and bin instead
+})
+    
+/* function to remove element with display none , param : your element */
+function displayNone(element){
+    element.classList.add("d-none");
+}
+
+/* function to dispaly element with removing display none , param : your element */
+function display(element){
+    element.classList.remove("d-none");
 }
