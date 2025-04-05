48f26540bdb3c1d9cee235e4b8dd96bf5eff1a46
sayedali1 <said.ali199859@gmail.com>
2025-04-05 10:37:14 +0200

    img display

--- AdminPages/JS/addcategory.js
+++ AdminPages/JS/addcategory.js
@@ -34,7 +34,21 @@ async function uploadToImgur(imageFile, clientId) {
     }
   }
 
+  const setUrl = document.getElementById("preview-img")
+  setUrl.src = "https://imgur.com/AhyEeor.png"
+  document.getElementById("img_icon").style.display = "none"; // Hide the icon
+  setUrl.style.display = "inline"; // Show the uploaded image
+
 document.getElementById("file-input").addEventListener("change",async() =>{
+
+    const currentImageUrl = document.getElementById("preview-img").src;
+
+    if (currentImageUrl && currentImageUrl !== "") {
+        console.log("Image already exists:", currentImageUrl);
+    }
+    else{
+        
+    }
     const imageFile = document.getElementById("file-input").files[0]
     const clientId = "5c51da6457cf182"
 
@@ -42,8 +56,14 @@ document.getElementById("file-input").addEventListener("change",async() =>{
         const imageUrl = await uploadToImgur(imageFile, clientId);
         
         if (imageUrl) {
-          
           console.log("Image URL:", imageUrl);
+          const reader = new FileReader();
+            reader.onload = () => {
+                const previewImg = document.getElementById("preview-img");
+                previewImg.src = reader.result; // Set image source
+                previewImg.style.display = "inline"; // Show the uploaded image
+            };
+            reader.readAsDataURL(imageFile)
         } else {
           console.log("Upload failed!");
         }
