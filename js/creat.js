44c53fa205dd3e0abc1bf52dd5a1b0227a6f4068
samuelashraf <samuelashraf100@gmail.com>
2025-04-06 14:23:07 +0200

    hhhhhhh

---
+++ js/creat.js
@@ -0,0 +1,60 @@
+import { addDocument } from "./main.js";
+import {
+  validateName,
+  validateEmail,
+  validatePassword,
+  validateRePassword,
+  validatePhoneNumber,
+} from "./CreatUser.js";
+
+let CreatAcount = document.getElementById("Creataccount_id");
+CreatAcount.addEventListener("click", (e) => {
+  e.preventDefault();
+  let name = document.getElementById("userName_id").value;
+  let email = document.getElementById("email_id").value;
+  let password = document.getElementById("password_id").value;
+  let rePassword = document.getElementById("repassword_id").value;
+  let phoneNumber = document.getElementById("phoneNumber_id");
+
+  if (
+    validateName(name) &&
+    validateEmail(email) &&
+    validatePassword(password) &&
+    validateRePassword(password, rePassword) &&
+    validatePhoneNumber(phoneNumber)
+  ) {
+    let dataUser = {
+      isAdmin: false,
+      name,
+      password,
+      email,
+      phoneNumber,
+      address: {
+        country: "",
+        city: "",
+      },
+      wishlist: [],
+      shoppingCart: [
+        {
+          cat_id: "",
+          quantaty: "",
+          isPending: "",
+        },
+      ],
+      lastOrders: [
+        {
+          product_id: "",
+          retunOdrs: false,
+        },
+      ],
+    };
+    addDocument("User", dataUser)
+      .then(() => {
+        window.location.href = "../index.html";
+      })
+      .catch((error) => {
+        console.error("Error creating account:", error);
+        alert("There was an error creating your account. Please try again.");
+      });
+  }
+});
